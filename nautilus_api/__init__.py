4349428618de0ea9c2ce707c233b52df4afb3baf
Arshan <111618520+ArshansGithub@users.noreply.github.com>
2025-03-11 17:40:46 -0700

    dont tell anyone

--- nautilus_api/__init__.py
+++ nautilus_api/__init__.py
@@ -138,6 +138,11 @@ def create_app():
             return ":)"
         
         return "greetings curious one"
+    
+    @app.route("/migrate")
+    async def migrate():
+        await migrate_user_api_version()
+        return "started"
 
     # Register API routes
     app.register_blueprint(account_routes.account_api, url_prefix="/api/account")
@@ -145,7 +150,5 @@ def create_app():
     app.register_blueprint(attendance_routes.attendance_api, url_prefix="/api/attendance")
     app.register_blueprint(meeting_routes.meeting_api, url_prefix="/api/meetings")
     app.register_blueprint(notification_routes.notification_api, url_prefix="/api/notifications")
-
-    migrate_user_api_version()
 
     return app
